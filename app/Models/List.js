be7724c81d9d1b62e5fa8b40045a312a2364ec30
KorbinWilliams <52514607+KorbinWilliams@users.noreply.github.com>
2019-11-22 11:55:42 -0700

    alright now the models are good?

--- app/Models/List.js
+++ app/Models/List.js
@@ -1,11 +1,11 @@
 import { generateId } from "../utils.js";
+import ListItems from "./ListItem.js"
 
 export default class List {
-  constructor(data) {
-    //TODO Your constructor takes in a data object that should have the properties you need to create your list here is a freebie, it will set the id its provided, or if that is undefined it will create a new one (this is an alternative to object destructuring)
-    this.id = data.id || generateId();
-    this.listname = this.listname
-    this.listItem = this.listItem.map(l => l = new this.listItem(l));
+  constructor({ id, listname, listitem }) {
+    this.id = id || generateId();
+    this.listname = listname
+    this.listitem = listitem.map(l => l = new ListItems(l));
   }
   //Be sure to add the methods needed to create the view template for this model
   //For starting out, your tasks may be strings alone, but later you may wish to turn them into full objects, that will be up to you
--- app/Models/ListItem.js
+++ app/Models/ListItem.js
@@ -1,11 +1,14 @@
+import { generateId } from "../utils"
+
 export default class ListItems {
-  constructor(data) {
-    this.listItems = listItems
+  constructor({ id = generateId(), listItem }) {
+    this.id = id
+    this.listItem = listItem
   }
 
   getTemplate() {
     return `
-    <h5>${this.listItems}</h5>
+    <h5>${this.listItem}</h5>
     `
   }
 }
